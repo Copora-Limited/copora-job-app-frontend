bcf389c279f9588a2229a82a851fd4acc3439029
Shafi Akinropo <sakinropo@gmail.com>
2024-11-06 13:43:12 +0100

    Reverse

--- components/onboardingForm/PersonalDetails-deprecated.js
+++ components/onboardingForm/PersonalDetails-deprecated.js
@@ -3,9 +3,9 @@ import Image from "next/image";
 import { useSession } from "next-auth/react";
 import { useSessionContext } from "@/context/SessionContext";
 import UploadBtn from "@/components/Custom/Buttons/UploadBtn";
-
 import { UploadIcon } from "@/components/Icon";
 import UploadedFile from "@/components/Custom/UploadedFile";
+import OptionsComponent from "@/components/GeneralInformation/OptionComponentNew";
 
 import { CircleSpinnerOverlay } from "react-spinner-overlay";
 
@@ -26,8 +26,8 @@ const PersonalDetails = ({ onChange }) => {
     addressProof: useRef(null),
     photo: useRef(null),
   };
-  const [nin, setNin] = useState(Array(9).fill(""));
-  const [declarationAccepted, setDeclarationAccepted] = useState(false);
+  const [nin, setNin] = useState(Array(9).fill("")); // Initialize NIN array with 9 empty strings
+  const [requireWorkVisa, setRequireWorkVisa] = useState("false");
 
   useEffect(() => {
     setIsMounted(true);
@@ -54,9 +54,10 @@ const PersonalDetails = ({ onChange }) => {
         setLocalFormData(data);
         setProfilePic(data.passportPhoto || "");
 
-        setNin(data.nationalInsuranceNumber?.split("") || Array(9).fill(""));
+        setNin(data.nationalInsuranceNumber?.split("") || Array(9).fill("")); // Split NIN into digits
         onChange(data);
-        setHasFetchedData(true);
+        setRequireWorkVisa(data.requireWorkVisa);
+        setHasFetchedData(true); // Only set data once
         setIsLoading(false);
       } catch (error) {
         console.error("Error fetching applicant data:", error);
@@ -74,21 +75,26 @@ const PersonalDetails = ({ onChange }) => {
     onChange(updatedFormData);
   };
 
-  const handleFileChange = (type) => (e) => {
+  const handleFileChange = (e) => {
     const file = e.target.files?.[0];
     if (file) {
-      const updatedFormData = { ...localFormData, [type]: file };
+      setProfilePic(URL.createObjectURL(file));
+      const updatedFormData = { ...localFormData, passportPhoto: file };
       setLocalFormData(updatedFormData);
       onChange(updatedFormData);
     }
   };
 
+  const handleClick = () => {
+    fileRef.current?.click();
+  };
+
   const handleNinChange = (index, e) => {
-    const value = e.target.value.toUpperCase();
+    const value = e.target.value.toUpperCase(); // Ensure the NIN is in uppercase
     const isValidInput =
-      (index < 2 && /^[A-Za-z]?$/.test(value)) ||
-      (index >= 2 && index < 8 && /^\d?$/.test(value)) ||
-      (index === 8 && /^[A-Za-z]?$/.test(value));
+      (index < 2 && /^[A-Za-z]?$/.test(value)) || // First 2 inputs: Alphabets only
+      (index >= 2 && index < 8 && /^\d?$/.test(value)) || // Next 6 inputs: Numbers only
+      (index === 8 && /^[A-Za-z]?$/.test(value)); // Last input: Alphabet only
 
     if (isValidInput && value.length <= 1) {
       const newNin = [...nin];
@@ -98,14 +104,24 @@ const PersonalDetails = ({ onChange }) => {
       setLocalFormData({ ...localFormData, nationalInsuranceNumber });
       onChange({ ...localFormData, nationalInsuranceNumber });
 
+      // Move focus to the next input if the current one is filled
       if (value && index < nin.length - 1) {
         document.getElementById(`nin-${index + 1}`).focus();
       }
     }
   };
 
-  const handleClick = (ref) => () => {
-    ref.current?.click();
+  const handleUploadClick = (inputId) => {
+    document.getElementById(inputId).click();
+  };
+
+  const handleUploadFileChange = (event, fileType) => {
+    const file = event.target.files[0];
+    if (file) {
+      const updatedFormData = { ...localFormData, [fileType]: file };
+      setLocalFormData(updatedFormData);
+      onChange(updatedFormData);
+    }
   };
 
   const renderUploadSection = (fileType, title, id) => {
@@ -155,7 +171,7 @@ const PersonalDetails = ({ onChange }) => {
               type="file"
               id={id}
               className="hidden"
-              onChange={(event) => handleFileChange(event, fileType)} // Handle file change
+              onChange={(event) => handleUploadFileChange(event, fileType)} // Handle file change
               accept=".pdf, docx, .doc, .png, .jpg, .jpeg"
             />
           </div>
@@ -164,6 +180,14 @@ const PersonalDetails = ({ onChange }) => {
     );
   };
 
+  const handleCheckboxChange = (name, value) => {
+    const updatedFormData = { ...localFormData, [name]: value };
+    setLocalFormData(updatedFormData);
+    onChange(updatedFormData);
+    if (name === "requireWorkVisa") {
+      setRequireWorkVisa(value); // Set the requireWorkVisa state
+    }
+  };
   return (
     <>
       {isMounted && (
@@ -172,20 +196,16 @@ const PersonalDetails = ({ onChange }) => {
           overlayColor="rgba(0,153,255,0.2)"
         />
       )}
-      <div className="mt-8">
-        <label className="block text-sm font-medium text-gray-700 mt-4 mb-1">
-          Submit a recent, passport-style headshot or a selfie featuring a
-          professional smile.
-        </label>
-      </div>
-      <div className="w-full flex items-center gap-3">
+
+      <div className="w-full flex items-center gap-3 mt-8">
         <input
           type="file"
           hidden
           ref={fileRef}
-          onChange={handleFileChange("profilePic")}
+          onChange={handleFileChange}
           accept="image/*"
         />
+
         <div className="relative w-[60px] h-[60px] rounded-full">
           <Image
             src={profilePic || "/assets/img/user-avatar.svg"}
@@ -203,10 +223,15 @@ const PersonalDetails = ({ onChange }) => {
             placeholder="empty"
           />
         </div>
-        <UploadBtn
-          text="Upload Passport-Style Photo"
-          onClick={handleClick(fileRef)}
-        />
+
+        <div className="">
+          <UploadBtn text="Upload Passport" onClick={handleClick(fileRef)} />
+          <div className="text-[12px] font-azoSansRegular my-2">
+            {" "}
+            Submit a recent, passport-style headshot or a selfie featuring a
+            professional smile.
+          </div>
+        </div>
       </div>
 
       <div className="my-10 grid grid-cols-1 md:grid-cols-2 gap-4">
@@ -259,10 +284,12 @@ const PersonalDetails = ({ onChange }) => {
           >
             National Insurance Number
           </label>
+
           <p className="text-[12px] font-azoSansRegular">
-            This will be in your National Insurance letter, payslip, or P60. For
+            This will be in you National Insurance letter, payslip or P60. For
             example, 'QQ 12 34 56 C'
           </p>
+
           <div className="grid grid-cols-9 gap-2 mt-2">
             {nin.map((digit, index) => (
               <input
@@ -272,16 +299,108 @@ const PersonalDetails = ({ onChange }) => {
                 maxLength={1}
                 value={digit}
                 onChange={(e) => handleNinChange(index, e)}
-                placeholder={index < 2 ? "A" : index < 8 ? "0" : "A"}
+                placeholder={
+                  index < 2 ? "A" : index < 8 ? "0" : "A" // Set appropriate placeholder
+                }
                 className="w-10 h-10 text-center border rounded"
               />
             ))}
           </div>
         </div>
+
+        <div className="">
+          <label
+            htmlFor="passport"
+            className="block text-sm font-medium text-gray-700"
+          >
+            Proof of National Insurance Number
+          </label>
+          {renderUploadSection(
+            "ninProof",
+            "Proof of National Insurance Number",
+            "fileInputninproof"
+          )}
+        </div>
       </div>
 
-      {/* Additional sections for Passport, NIN proof, Address proof, etc. */}
-      {/* Include the additional upload buttons and instructions as outlined before */}
+      {/* Proof of Address Section */}
+      <div className="">
+        <label
+          htmlFor="addressProof"
+          className="block text-sm font-medium text-gray-700"
+        >
+          Proof of address
+        </label>
+        <p className="text-[12px] font-azoSansRegular">
+          Provide either a recent bank statement or an official
+          government-issued letter.
+        </p>
+
+        <div className="w-full grid md:grid-cols-2 grid-cols-1 gap-4">
+          {renderUploadSection(
+            "addressProof",
+            "Proof of Address",
+            "fileInputAddressProof"
+          )}
+        </div>
+      </div>
+
+      <div className="w-full flex items-center gap-3 my-10 ">
+        <OptionsComponent
+          title="Do you require a work visa to work in the resident country?"
+          isCheckedLeft={localFormData.requireWorkVisa === "true"}
+          setIsCheckedLeft={() =>
+            handleCheckboxChange("requireWorkVisa", "true")
+          }
+          isCheckedRight={localFormData.requireWorkVisa === "false"}
+          setIsCheckedRight={() =>
+            handleCheckboxChange("requireWorkVisa", "false")
+          }
+          idLeft="RequireVisaYes"
+          idRight="RequireVisaNo"
+        />
+      </div>
+
+      {requireWorkVisa === "true" && (
+        <div className=" grid grid-cols-1 md:grid-cols-2 gap-4">
+          <div className="">
+            <label
+              htmlFor="visaDocument"
+              className="block text-sm font-medium text-gray-700"
+            >
+              Visa Document
+            </label>
+            <p className="text-[12px] font-azoSansRegular">
+              Upload your visa document for verification
+            </p>
+
+            <div className="">
+              {renderUploadSection(
+                "visaDocument",
+                "Visa Document",
+                "fileInputvisa"
+              )}
+            </div>
+          </div>
+
+          <div className="">
+            <label
+              htmlFor="internationalPassport"
+              className="block text-sm font-medium text-gray-700"
+            >
+              International Passport
+            </label>
+            <p className="text-[12px] font-azoSansRegular">
+              Upload Data page of your International Passport
+            </p>
+            {renderUploadSection(
+              "internationalPassport",
+              "International Passport",
+              "fileInputPassPort"
+            )}
+          </div>
+        </div>
+      )}
     </>
   );
 };
