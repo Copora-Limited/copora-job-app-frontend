55dd57b5a9c0e579daa8d65d2f1b229576e85330
Shafi Akinropo <sakinropo@gmail.com>
2024-11-18 07:38:37 +0100

    Update reset-password.js

--- pages/auth/reset-password.js
+++ pages/auth/reset-password.js
@@ -105,7 +105,7 @@ const ResetPassword = () => {
               placeholder="Password"
               value={password}
               onChange={(e) => setPassword(e.target.value)}
-              className={`w-full px-3 py-2 border rounded-full text-lg focus:outline-none focus:ring-2 ${
+              className={`outline-none border-none w-full h-full placeholder:text-gray-400 text-sm ${
                 isDarkMode
                   ? "bg-gray-700 border-gray-600 text-white focus:ring-cyan-600"
                   : "bg-white border-gray-300 text-gray-900 focus:ring-cyan-500"
@@ -130,7 +130,7 @@ const ResetPassword = () => {
               placeholder="Confirm Password"
               value={confirmPassword}
               onChange={(e) => setConfirmPassword(e.target.value)}
-              className={`w-full px-3 py-2 border rounded-full text-lg focus:outline-none focus:ring-2 ${
+              className={`outline-none border-none w-full h-full placeholder:text-gray-400 text-sm ${
                 isDarkMode
                   ? "bg-gray-700 border-gray-600 text-white focus:ring-cyan-600"
                   : "bg-white border-gray-300 text-gray-900 focus:ring-cyan-500"
@@ -151,7 +151,7 @@ const ResetPassword = () => {
           </div>
           <button
             type="submit"
-            className={`w-full p-2 rounded bg-secondary text-white hover:bg-teal-700 transition ${
+            className={`w-full p-2 rounded bg-appGreen hover:bg-teal-700 transition duration-500 text-white ${
               loading ? "opacity-50 cursor-not-allowed" : ""
             }`}
             disabled={loading}
